3ba3b95a264f1a5c409f8cf12a4133b9ed8ec67a
caixiaoyong <771802291@qq.com>
2020-11-25 12:57:12 +0800

    TextView

--- app/src/main/java/com/example/cy/hello/MainActivity.java
+++ app/src/main/java/com/example/cy/hello/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.cy.hello;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Button mBtnTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnTextView =findViewById(R.id.btn_textview);
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到TestView演示界面
+                Intent intent=new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
