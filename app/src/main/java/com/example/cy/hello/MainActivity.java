f1b90b3e701f0205027d7a0d4a26db5a5c98ee41
caixiaoyong <771802291@qq.com>
2020-11-26 21:51:40 +0800

    Button、EditText

--- app/src/main/java/com/example/cy/hello/MainActivity.java
+++ app/src/main/java/com/example/cy/hello/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
     private Button mBtnTextView;
+    private Button mBtnButton;
+    private Button mBtnEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,24 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 //跳转到TestView演示界面
                 Intent intent=new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBtnButton=findViewById(R.id.btn_button);
+        mBtnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到Button演示界面
+                Intent intent=new Intent(MainActivity.this,ButtonActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBtnEditText=findViewById(R.id.btn_edittext);
+        mBtnEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到EditText演示界面
+                Intent intent=new Intent(MainActivity.this,EditTextActivity.class);
                 startActivity(intent);
             }
         });
